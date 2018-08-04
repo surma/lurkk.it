bc0ba5e9f9fb8fece6f81d04b29719239e08d2f0
Surma <surma@surma.link>
2018-08-04 01:39:28 +0100

    feat(codesplitloader): turn amd loader into rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,57 +3,38 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import css from "./utils/rollup-plugin-css";
 import markup from "./utils/rollup-plugin-markup";
-import amdRename from "./utils/rollup-plugin-amd-rename";
+import codesplitloader from "./utils/rollup-plugin-codesplitloader/index.js";
+
+import { readFileSync } from "fs";
 
 // Delete 'dist'
 require("rimraf").sync("dist");
 
-export default [
-  {
-    input: ["src/bootstrap.ts", "src/worker.ts", "src/sw.ts"],
-    output: {
-      dir: "dist",
-      format: "amd",
-      sourcemap: process.env.SOURCEMAPS ? "inline" : false
-    },
-    plugins: [
-      typescript({
-        clean: true,
-        // Make sure we are using our version of TypeScript.
-        typescript: require("typescript"),
-        tsconfigOverride: {
-          compilerOptions: {
-            sourceMap: true
-          }
-        }
-      }),
-      nodeResolve(),
-      terser(),
-      css(),
-      markup(),
-      amdRename()
-    ],
-    experimentalCodeSplitting: true
+export default {
+  input: ["src/bootstrap.ts", "src/worker.ts", "src/sw.ts"],
+  output: {
+    dir: "dist",
+    format: "amd",
+    sourcemap: process.env.SOURCEMAPS ? "inline" : false
   },
-  {
-    input: "utils/amd-loader.ts",
-    output: {
-      file: "dist/loader.js",
-      format: "iife",
-      sourcemap: process.env.SOURCEMAPS ? "inline" : false
-    },
-    plugins: [
-      typescript({
-        clean: true,
-        // Make sure we are using our version of TypeScript.
-        typescript: require("typescript"),
-        tsconfigOverride: {
-          compilerOptions: {
-            sourceMap: true
-          }
+  plugins: [
+    typescript({
+      clean: true,
+      // Make sure we are using our version of TypeScript.
+      typescript: require("typescript"),
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: true
         }
-      }),
-      terser()
-    ]
-  }
-];
+      }
+    }),
+    nodeResolve(),
+    css(),
+    markup(),
+    codesplitloader({
+      loader: readFileSync("./utils/rollup-plugin-codesplitloader/loader.js")
+    })
+    // terser(),
+  ],
+  experimentalCodeSplitting: true
+};
